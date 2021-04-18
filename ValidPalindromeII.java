dd46c6708277c1e1fe4778be20ea2aa2cf9b0291
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2021-04-18 16:22:32 -0700

    Create ValidPalindromeII.java

---
+++ ValidPalindromeII.java
@@ -0,0 +1,12 @@
+// 125. Valid Palindrome
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        for (int i=0; i<s.length(); i++){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
